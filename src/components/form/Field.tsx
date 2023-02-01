d6a1c8d3475120e1c4e2146f4be9a62e16aeb6bb
Barry Jacobs <barry.jacobs@mechenet.com>
2023-02-01 07:21:37 +0000

    Fixes for error highlighting

--- src/components/form/Field.tsx
+++ src/components/form/Field.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react"
 import { CodeEditor } from "components"
 import TextareaAutosize from "react-textarea-autosize"
 
@@ -15,22 +14,21 @@ interface FieldProps {
 
 const renderLabel = (fieldName: string, labelText: string) => {
   return (
-    <label htmlFor={fieldName} className="text-gray-600 font-medium text-md">
+    <label htmlFor={fieldName} className="text-md font-medium text-gray-600">
       {labelText}
     </label>
   )
 }
 
 export const Field = ({ type, name, label, hint, error, ...props }: FieldProps) => {
-  const renderField = useMemo(() => {
-    const borderColor = error ? "red" : "gray"
+  const renderField = (borderColor: string) => {
     switch (type) {
       case "text":
         return (
           <>
             {renderLabel(name, label)}
             <input
-              className={`outline-none focus:outline-none text-md bg-white rounded-md px-3 py-2 w-full border-2 focus:border-${borderColor}-400 border-${borderColor}-300 font-regular mt-1 transition-all`}
+              className={`text-md font-regular mt-1 w-full rounded-md border-2 border-${borderColor}-300 bg-white px-3 py-2 outline-none transition-all focus:border-${borderColor}-400 focus:outline-none`}
               {...props}
             />
           </>
@@ -40,7 +38,7 @@ export const Field = ({ type, name, label, hint, error, ...props }: FieldProps) 
           <>
             {renderLabel(name, label)}
             <TextareaAutosize
-              className={`outline-none focus:outline-none text-md bg-white rounded-md px-3 py-2 w-full border-2 focus:border-${borderColor}-400 border-${borderColor}-300 font-regular mt-1 transition-all`}
+              className={`text-md w-full rounded-md border-2 bg-white px-3 py-2 outline-none focus:outline-none focus:border-${borderColor}-400 border-${borderColor}-300 font-regular mt-1 transition-all`}
               {...props}
             />
           </>
@@ -57,11 +55,12 @@ export const Field = ({ type, name, label, hint, error, ...props }: FieldProps) 
       default:
         null
     }
-  }, [error, label, name, props, type])
+  }
 
+  const borderColor = error ? "red" : "gray"
   return (
     <div className="mt-4">
-      {renderField}
+      {renderField(borderColor)}
       {error ? (
         <div className={`text-xs text-red-600 mt-${type === "textarea" ? "0" : "1"}`}>{error}</div>
       ) : (
